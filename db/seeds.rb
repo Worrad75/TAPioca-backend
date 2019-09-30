0f1d1c1f412b87fd71a381870925fdc6192d98df
github username <github email address>
2019-09-30 16:32:32 -0400

    got backend working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+# Score.destroy_all
+# Game.destroy_all
+
+user_a = User.create!(username: "Darrow", password_digest: "abc")
+user_b = User.create!(username: "Belinda", password_digest: "abc")
+user_b = User.create!(username: "Daniel", password_digest: "abc")
+puts "done making users"
+
+# score_a = Score.create!(tally: 250)
+# score_b = Score.create!(tally: 350)
+# score_c = Score.create!(tally: 450)
+# puts "done making scores"
+
+# game_a = Game.create!(score: score_a, user: user_a)
+# game_b = Game.create!(score_id: score_b.id, user_id: user_b.id)
+# game_b = Game.create!(score_id: score_b.id, user_id: user_b.id)
+# puts "done making games"

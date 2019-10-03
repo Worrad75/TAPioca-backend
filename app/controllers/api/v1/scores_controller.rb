36f5e1f6262279fd31500c6f68c92736566b7e27
github username <github email address>
2019-10-03 13:21:05 -0400

    adjusted create to send back game data

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -5,6 +5,19 @@ class Api::V1::ScoresController < ApplicationController
     render json: @scores
   end
 
+  def create
+    score = Score.new(score_params)
+
+    if score.save
+      user = User.find_by(username: params[:username])
+      game = Game.create(user_id: user.id, score_id: score.id) if user
+      render json: {score: score, game: game}
+    else 
+      render json: {errors: score.errors.full_messages}
+    end
+
+  end
+
   def destroy
     score = Score.find(params[:id])
     score.destroy
@@ -12,4 +25,9 @@ class Api::V1::ScoresController < ApplicationController
     render json: {message: "Successfully deleted"}
   end
 
+  private
+
+  def score_params
+    params.require(:score).permit(:tally)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -37,6 +37,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username)
     end
 end

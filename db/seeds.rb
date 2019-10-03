9f1bc37bb4917faa92e9491f71bc8f0ac239c5a4
github username <github email address>
2019-10-03 09:27:33 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,15 @@ Game.destroy_all
 
 user_a = User.create!(username: "Darrow", password_digest: "abc")
 user_b = User.create!(username: "Belinda", password_digest: "abc")
-user_b = User.create!(username: "Daniel", password_digest: "abc")
+user_c = User.create!(username: "Daniel", password_digest: "abc")
 puts "done making users"
 
-score_a = Score.create!(tally: 250)
-score_b = Score.create!(tally: 350)
+score_a = Score.create!(tally: 350)
+score_b = Score.create!(tally: 250)
 score_c = Score.create!(tally: 450)
 puts "done making scores"
 
-game_a = Game.create!(score: score_a, user: user_a)
+game_a = Game.create!(score_id: score_a.id, user_id: user_a.id)
 game_b = Game.create!(score_id: score_b.id, user_id: user_b.id)
-game_b = Game.create!(score_id: score_b.id, user_id: user_b.id)
+game_c = Game.create!(score_id: score_c.id, user_id: user_c.id)
 puts "done making games"
